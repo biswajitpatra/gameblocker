9f0ffbc642e5941c84f11cf7bcdfa349cf743b00
rocky5662 <35865870+rocky5662@users.noreply.github.com>
2018-11-05 17:57:08 +0530

    Update and rename app to app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+server_date=''
+server_min=''
+
+@app.route('/server/<date>/<min>')
+def server_side(date,min):
+   global server_date
+   global server_min
+   if min=='0':
+     if server_date==date:
+       return "APP "+min
+     else:
+       return "NOTA"
+   server_date=date
+   server_min=min
+   return 'DONE'
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
